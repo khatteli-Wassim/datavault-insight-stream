a29dbddbb651d69f79e2cef092a07ed2b3b522b5
vaken <khatteli-Wassim@users.noreply.github.com>
2025-04-02 14:30:36 +0000

    chore: add basic folder structure for dashboard components

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>Datavault Insight Stream</h1>
+        </header>
+    );
+};
+
+export default Header;
